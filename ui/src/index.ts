803c2b7b9a4e2debd129679072314e1d5b2b82fc
AirboZH <i@airbozh.cn>
2024-07-12 00:30:05 +0800

    init commit

---
+++ ui/src/index.ts
@@ -0,0 +1,29 @@
+import { definePlugin } from "@halo-dev/console-shared";
+import HomeView from "./views/HomeView.vue";
+import { markRaw } from "vue";
+import {IconMotionLine} from "@halo-dev/components";
+
+export default definePlugin({
+  components: {},
+  routes: [
+    {
+      parentName: "Root",
+      route: {
+        path: "/sticker",
+        name: "Sticker",
+        component: HomeView,
+        meta: {
+          title: "表情管理",
+          searchable: true,
+          menu: {
+            name: "表情管理",
+            group: "system",
+            icon: markRaw(IconMotionLine),
+            priority: 0,
+          },
+        },
+      },
+    },
+  ],
+  extensionPoints: {},
+});
