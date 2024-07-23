478c6721e7336854239365c34cfd3095f104ef24
AirboZH <i@airbozh.cn>
2024-07-23 16:36:49 +0800

    chore: remove useless code

--- src/main/java/run/halo/sticker/StickerPlugin.java
+++ src/main/java/run/halo/sticker/StickerPlugin.java
@@ -1,17 +1,9 @@
-package run.halo.starter;
+package run.halo.sticker;
 
 import org.springframework.stereotype.Component;
 import run.halo.app.plugin.BasePlugin;
 import run.halo.app.plugin.PluginContext;
 
-/**
- * <p>Plugin main class to manage the lifecycle of the plugin.</p>
- * <p>This class must be public and have a public constructor.</p>
- * <p>Only one main class extending {@link BasePlugin} is allowed per plugin.</p>
- *
- * @author guqing
- * @since 1.0.0
- */
 @Component
 public class StickerPlugin extends BasePlugin {
 
